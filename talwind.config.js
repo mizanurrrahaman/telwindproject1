7ccf007113031efb5241f358f90737ce54c2e0af
mizanurrrahaman <rodking1414@gmail.com>
2023-10-08 06:19:46 -0700

    telwind project

---
+++ talwind.config.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+    theme: {
+      extend: { 
+         backgroundImage: {
+          'banner': "url('images/banner.jpg')",
+          'project': "url('images/project.png')",
+        
+      },
+        colors: {
+          clifford: '#da373d',
+        },
+         maxWidth: {
+            'container': '1920px',
+          },
+          fontFamily: {
+            'poppinse': "Poppins, sans-serif;",
+          },
+        
+      }
+    }
+  }
